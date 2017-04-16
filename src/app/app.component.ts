8240e2b19628a38e1cde0e389b585f531d926e80
amanpatial <aman.patial@gmail.com>
2017-04-17 00:31:19 +0530

    add basic style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'weather-app-root',
  // template: `<h1> Angular2 Weather App!</h1><app-weather></app-weather>`,
   templateUrl:'./app.component.html',
-  styleUrls: ['./app.component.css']
+ // styleUrls: ['./app.component.css']
+ //styleUrls: ['../style.css']
 })
 export class AppComponent {
   title = 'Angular2, Weather app works!';
